aa0152b97aad12b4763effa855fe3b095ac2ec68
mnineteenv <90570875+m19v@users.noreply.github.com>
2022-08-27 21:05:39 +0200

    MusingsComponent adjusted

--- src/components/MusingsComponent/index.js
+++ src/components/MusingsComponent/index.js
@@ -5,32 +5,26 @@ const MusingList = [
   {
     text: "Half knowledge is worse than ignorance.",
     author: "Thomas B. Macaulay",
-    date: "January 19, 2019",
   },
   {
     text: "You get in life what you have the courage to ask for.",
     author: "Nancy D. Solomon",
-    date: "August 04, 2014",
   },
   {
     text: "You can tell how smart people are by the things they laugh at.",
     author: "Tina Fey",
-    date: "February 27, 2014",
   },
   // {
   //   text: "",
   //   author: "",
-  //   date: "",
   // },
 ];
 
-function Musing({ text, author, date }) {
+function Musing({ text, author }) {
   return (
     <div className={styles.musingsContainer}>
       <span className={styles.musingsText}>{text}</span>
-      <span> - </span>
-      <span className={styles.musingsAuthor}>{author}</span>
-      <div className={styles.musingsDate}>{date}</div>
+      <div className={styles.musingsAuthor}>{author}</div>
     </div>
   );
 }
