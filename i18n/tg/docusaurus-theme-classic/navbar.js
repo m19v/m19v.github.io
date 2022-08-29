f801a541c629067fa6fda14b3dcd7e2ef240990b
mnineteenv <90570875+m19v@users.noreply.github.com>
2022-08-29 23:04:17 +0200

    navbar translation file added

---
+++ i18n/tg/docusaurus-theme-classic/navbar.js
@@ -0,0 +1,18 @@
+{
+    "title": {
+      "message": "",
+      "description": "The title in the navbar"
+    },
+    "item.label.Docs": {
+      "message": "Ҳуҷҷатгузорӣ",
+      "description": "Navbar item with label Docs"
+    },
+    "item.label.Blog": {
+      "message": "Вебнавишт",
+      "description": "Navbar item with label Blog"
+    },
+    "item.label.About": {
+      "message": "Муаллиф",
+      "description": "Navbar item with label About"
+    }
+}
