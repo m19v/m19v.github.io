7c54d093caec3aee5401655027035eaf84a70b53
mnineteenv <mnineteenv@gmail.com>
2023-11-19 21:24:41 +0100

    feat: upgrade docusaurus to 3.0.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 const math = require('remark-math');
 const katex = require('rehype-katex');
@@ -153,8 +154,9 @@ const config = {
         copyright: `© ${new Date().getFullYear()} - {m19v}`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        additionalLanguages: ['bash', 'diff', 'json'],
       },
       docs: {
         sidebar: {
